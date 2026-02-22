011914438faeda0daa76375db706d8860a453841
batminson <62904927+batminson@users.noreply.github.com>
2026-02-21 21:26:14 -0500

    Update db.php

--- db.php
+++ db.php
@@ -2,10 +2,11 @@
 $db = new SQLite3(__DIR__ . '/tickets.db');
 
 $db->exec("
-CREATE TABLE IF NOT EXISTS tickets (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    nombre TEXT,
-    email TEXT,
-    problema TEXT,
-    estado TEXT
-)");
+    CREATE TABLE IF NOT EXISTS tickets (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        nombre TEXT,
+        email TEXT,
+        problema TEXT,
+        estado TEXT
+    )
+");
