1da50172838cc2ad4ef537f57ca87f0312ebb034
batminson <62904927+batminson@users.noreply.github.com>
2026-02-22 22:03:37 -0500

    Update db.php

--- db.php
+++ db.php
@@ -10,7 +10,10 @@ try {
         "mysql:host=$host;dbname=$db;port=$port;charset=utf8",
         $user,
         $pass,
-        [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
+        [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        ]
     );
 } catch (PDOException $e) {
     die("Error de conexión: " . $e->getMessage());
